3b6dca51704ef82fec2e3c32ebd82c0ed95db7c7
Aleksei Chepurko <aichepurko@yandex-team.ru>
2019-03-23 22:25:23 +0300

    button atom

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ButtonComponent } from 'src/app/atoms/button/button.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ButtonComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
